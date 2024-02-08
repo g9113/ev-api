156b3da913ca09db949f070ee6dea52735547484
Sumeet Vishwakarma <85386116+LegendSumeet@users.noreply.github.com>
2024-02-08 19:40:46 +0530

    updated

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
+
 app.use("/api/bootcamps", bootcampRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/conferences", conferenceRoutes);
